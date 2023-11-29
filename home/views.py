fdfba5e2764b00bfdd56826d906e2ac3fb0678d3
Talis Eduardo Cipriani <tec-talis@MacBook-Air-de-Talis.local>
2023-11-29 18:46:10 -0300

    tela

--- home/views.py
+++ home/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 
-def index(request):
-    return render(request, 'login.html')
+def home(request):
+    if not 'user' in request.session:
+        messages.warning(
+            request, 'Efetue o login')
+        return render(request, 'login.html')
+    return render(request, 'home.html')
 
 def submit_login(request):
     messages.success(
         request, 'Login efetuado')
+    request.session['user'] = 'teste'
+    return redirect('home')
+
+def logout(request):
+    del request.session['user']
+    messages.success(
+        request, 'Logout efetuado')
     return render(request, 'login.html')
--- setup/urls.py
+++ setup/urls.py
@@ -6,7 +6,8 @@ from home.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name="index"),
+    path('', home, name="home"),
     path('submit_login', submit_login, name="submit_login"),
+    path('logout', logout, name="logout"),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
